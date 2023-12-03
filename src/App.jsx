47791b128aa28fb10ced953de74df7a10263d189
Ferdinand Ramdhani <ferdinram13@gmail.com>
2023-12-03 13:42:03 +0700

    fea - board n logic X O

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import { useState } from 'react';
 
-function App() {
-  return <></>;
+// SQUARE
+// eslint-disable-next-line react/prop-types
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+// BOARD ( 9 SQUARES )
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+
+    const nextSquare = squares.slice();
+    nextSquare[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+  };
+
+  return (
+    <div className="board">
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+  );
+};
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 }
 
-export default App;
+export default Board;
